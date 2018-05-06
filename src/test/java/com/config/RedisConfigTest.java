b4eda9170f27473d79270e9527a8c0751cdedde9
tangj <840391701@qq.com>
2018-05-06 18:22:51 +0800

    redis的整合

--- src/test/java/com/config/RedisConfigTest.java
+++ src/test/java/com/config/RedisConfigTest.java
@@ -1,15 +1,19 @@
 package com.config;
 
 import com.domain.UserVo;
+import com.service.RedisService;
 import com.util.RedisKeyUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.*;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
@@ -23,6 +27,24 @@ public class RedisConfigTest {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    @Resource
+    private ValueOperations<String,Object> valueOperations;
+
+    @Autowired
+    private HashOperations<String, String, Object> hashOperations;
+
+    @Autowired
+    private ListOperations<String, Object> listOperations;
+
+    @Autowired
+    private SetOperations<String, Object> setOperations;
+
+    @Autowired
+    private ZSetOperations<String, Object> zSetOperations;
+
+    @Resource
+    private RedisService redisService;
+
     @Test
     public void testObj() throws Exception{
         UserVo userVo = new UserVo();
@@ -30,13 +52,57 @@ public class RedisConfigTest {
         userVo.setName("测试dfas");
         userVo.setAge(123);
         ValueOperations<String,Object> operations = redisTemplate.opsForValue();
-//        redisTemplate.delete("com.domain.UserVo");
+        redisService.expireKey("name",20, TimeUnit.SECONDS);
         String key = RedisKeyUtil.getKey(UserVo.Table,"name",userVo.getName());
-//        operations.set(key,userVo);
         UserVo vo = (UserVo) operations.get(key);
         System.out.println(vo);
     }
 
+    @Test
+    public void testValueOption( )throws  Exception{
+        UserVo userVo = new UserVo();
+        userVo.setAddress("上海");
+        userVo.setName("jantent");
+        userVo.setAge(23);
+        valueOperations.set("test",userVo);
 
+        System.out.println(valueOperations.get("test"));
+    }
 
+    @Test
+    public void testSetOperation() throws Exception{
+        UserVo userVo = new UserVo();
+        userVo.setAddress("北京");
+        userVo.setName("jantent");
+        userVo.setAge(23);
+        UserVo auserVo = new UserVo();
+        auserVo.setAddress("n柜昂周");
+        auserVo.setName("antent");
+        auserVo.setAge(23);
+        setOperations.add("user:test",userVo,auserVo);
+        Set<Object> result = setOperations.members("user:test");
+        System.out.println(result);
+    }
+
+    @Test
+    public void HashOperations() throws Exception{
+        UserVo userVo = new UserVo();
+        userVo.setAddress("北京");
+        userVo.setName("jantent");
+        userVo.setAge(23);
+        hashOperations.put("hash:user",userVo.hashCode()+"",userVo);
+        System.out.println(hashOperations.get("hash:user",userVo.hashCode()+""));
+    }
+
+    @Test
+    public void  ListOperations() throws Exception{
+        UserVo userVo = new UserVo();
+        userVo.setAddress("北京");
+        userVo.setName("jantent");
+        userVo.setAge(23);
+//        listOperations.leftPush("list:user",userVo);
+//        System.out.println(listOperations.leftPop("list:user"));
+        // pop之后 值会消失
+        System.out.println(listOperations.leftPop("list:user"));
+    }
 }
